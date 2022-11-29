f2ff3797980da8d92aa0fb4631f03fbdd2b47506
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-11-29 13:14:29 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,22 +7,11 @@ importing requests module
 from requests import get
 
 
-def count_words(subreddit, word_list=[], after=None, cleaned_dict=None):
+def recurse(subreddit, hot_list=[], after=None):
     """
-    function that queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited by
-    spaces. Javascript should count as javascript, but java should not).
+    function that queries the Reddit API and returns a list containing the
+    titles of all hot articles for a given subreddit.
     """
-
-    temp = []
-
-    for i in word_list:
-        temp.append(i.casefold())
-
-    cleaned_word_list = list(dict.fromkeys(temp))
-
-    if cleaned_dict is None:
-        cleaned_dict = dict.fromkeys(cleaned_word_list)
 
     params = {'show': 'all'}
 
@@ -40,29 +29,17 @@ def count_words(subreddit, word_list=[], after=None, cleaned_dict=None):
         return None
 
     all_data = response.json()
-    raw1 = all_data.get('data').get('children')
-    after = all_data.get('data').get('after')
 
-    if after is None:
-        new = {k: v for k, v in cleaned_dict.items() if v is not None}
+    try:
+        raw1 = all_data.get('data').get('children')
+        after = all_data.get('data').get('after')
 
-        for k in sorted(new.items(), key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(k[0], k[1]))
+        if after is None:
+            return hot_list
 
-        return None
+        for i in raw1:
+            hot_list.append(i.get('data').get('title'))
 
-    for i in raw1:
-        title = i.get('data').get('title')
-
-        split_title = title.split()
-
-        split_title2 = [i.casefold() for i in split_title]
-
-        for j in split_title2:
-            if j in cleaned_dict and cleaned_dict[j] is None:
-                cleaned_dict[j] = 1
-
-            elif j in cleaned_dict and cleaned_dict[j] is not None:
-                cleaned_dict[j] += 1
-
-    count_words(subreddit, word_list, after, cleaned_dict)
+        return recurse(subreddit, hot_list, after)
+    except:
+        print("None")
